06c25011d85606631a0fbbe4e2b7ddf35a8ab00c
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2025-03-30 06:32:46 +0300

    Create native segwit and get pubkey

--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,9 @@
 # Create a native segwit address and get the public key from the address.
+
+# create address
+address=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress)
+
+# get pubkey
+pubkey=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getaddressinfo "$address" | jq -r ".pubkey")
+
+echo $pubkey
