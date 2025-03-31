983469d272117efe49636da346240962fc83edcb
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2025-03-31 10:42:15 +0300

    Retreive recievers address

--- submission/07.sh
+++ submission/07.sh
@@ -1,2 +1,8 @@
 # What is the receiver's address in this partially signed transaction?
 # transaction=cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA
+
+transaction=cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA
+
+address=$(bitcoin-cli -regtest -rpcwallet=btrustwallet decodepsbt "$transaction" | jq -r ".tx .vout[0] .scriptPubKey .address")
+
+echo $address
